cd08b27f692dea8ddafed9cca1ca04cde49a65c2
patricia <patricia.gsbcn@gmail.com>
2025-10-30 18:25:54 +0100

    pc3

---
+++ Exercici01.js
@@ -0,0 +1,10 @@
+//Permet iniciar la finestra Despertador.html en el que es mostri la hora actual
+let btn_mostra_despertador = document.getElementById("btn_mostra_despertador");
+let tanca = document.getElementById("btn_tanca_despertador");
+let ref_window;
+btn_mostra_despertador.onclick = function(){
+    ref_window = window.open("Despertador.html","Despertador","width=300px,heigth=300px,toolbar=no,scrollbars=no");
+}
+btn_tanca_despertador.onclick = function(){
+    ref_window.close();
+}
