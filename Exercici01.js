75b8fcfd1512aa75b9d85db0c10c4185963e276f
patricia <patricia.gsbcn@gmail.com>
2025-10-31 18:06:36 +0100

    higth and with

--- Exercici01.js
+++ Exercici01.js
@@ -4,7 +4,13 @@ let btn_mostra_despertador = document.getElementById("btn_mostra_despertador");
 let tanca = document.getElementById("btn_tanca_despertador");
 let ref_window;
 btn_mostra_despertador.onclick = function(){
-    ref_window = window.open("Despertador.html","Despertador","width=300px,heigth=300px,toolbar=no,scrollbars=no");
+    let altura=screen.availHeight /2;
+    let amplada=screen.availWidth;
+    let with_window=300;
+    let heigth_window=300;
+    ref_window = window.open("Despertador.html","Despertador",
+        `width=`+with_window+`px,heigth=`+heigth_window+`px,toolbar=no,scrollbars=no, top=`
+        +altura+`px, left=`+amplada`px`);
 }
 btn_tanca_despertador.onclick = function(){
     ref_window.close();
