0cce94319a6b3718c88b8413d5e32af8d2fefcab
patricia <patricia.gsbcn@gmail.com>
2025-11-13 18:25:33 +0100

    teoria

---
+++ Despertador.js
@@ -0,0 +1,52 @@
+document.body.style.backgroundColor = window.opener.colorFilla;
+//cridem a mostrarHora Filla de la finestra mare
+window.opener.mostrarHoraFilla();
+//Quan el despertador arribi a la hora establerta, fes vibrar el despertador
+window.setInterval(checkHoraAlarma, 1000);
+function checkHoraAlarma() {
+    let hora_alarma = document.getElementById("div_hora_alarma").innerText;
+    let hora_actual = document.getElementById("div_hora_actual").innerText;
+    if (hora_actual == hora_alarma && hora_actual != "") {
+        window.setInterval(function () {
+            window.document.body.style.backgroundColor = llista_colors[numeroColor % 4]; //para ahorrar un if hacemos agrupaciones de 4 numeros con el resto
+            numeroColor++;
+        }, 500)
+
+    }
+}
+let llista_colors = new Array();
+llista_colors[0] = "yellow";
+llista_colors[1] = "orange";
+llista_colors[2] = "pink";
+llista_colors[3] = "red";
+let numeroColor = 0;
+
+let sta_numeros = new Array(5,3,"Hola");
+llista_numeros.length=10; //añade las posiciones que faltan vacias hasta llegar a 10
+llista_numeros[20]="ultim"; //posicion 20 añado 1
+llista_numeros.length=3; //se elimina todo hasta que solo tenga 3 posiciones
+llista_numeros[9]=new Array("Jose","Lito");
+console.log(llista_numeros); //nos muestra lo que hay en el array
+console.log(llista_numeros[9][1]); //veremos Lito
+
+let llista_valors = ["hello","array"];
+console.log(llista_valors);
+llista_valors[1] = ["Maria", "Antonia"]; // me crea un array en la posicion 1 donde tenia un string 
+
+for(let k=0; k < llista_numeros.length;k++){
+    console.log(k+"-"+llista_numeros[k]);
+}
+
+llista_numeros.forEach( function(value,index){
+    console.log(index+"::"+value);
+});
+
+llista_numeros["titol"]=["llista numeros"];
+llista_numeros["modul"]=" entorn client";
+console.log(llista_numeros);
+console.log(llista_numeros["titol"]);/*Un objeto solo tiene funciones y atributos
+//A cualquier objeto le puedo crear nuevos atributos */
+console.log(llista_numeros.titol); /*Un objeto solo tiene funciones y atributos
+//A cualquier objeto le puedo crear nuevos atributos */
+//En un array los numeros son enteros de los indices
+//
--- Exercici01.js
+++ Exercici01.js
@@ -1,7 +1,7 @@
 //Permet iniciar la finestra Despertador.html en el que es mostri la hora actual
 //https://www.w3schools.com/jsref/met_win_open.asp
 let btn_mostra_despertador = document.getElementById("btn_mostra_despertador");
-let tanca = document.getElementById("btn_tanca_despertador");
+let btn_tanca_despertador = document.getElementById("btn_tanca_despertador");
 let ref_window;
 btn_mostra_despertador.onclick = function(){
     let altura=screen.availHeight /2;
@@ -10,9 +10,9 @@ btn_mostra_despertador.onclick = function(){
     let height_window=300;
     ref_window = window.open("Despertador.html","Despertador",
         `width=`+width_window+`px,height=`+height_window+`px,toolbar=no,scrollbars=no, top=`
-        +(altura-height_window/2)+`px, left=`+(amplada-width_window/2)`px`);
+        +(altura-height_window/2)+`px, left=`+(amplada-width_window/2)+`px`);
     
-    document.setTimeout(function(){
+    windown.setTimeout(function(){
         ref_window.document.body.style.backgroundColor="blue";
     }, 1000 );
 }
@@ -22,9 +22,22 @@ btn_tanca_despertador.onclick = function(){
 
 const btn_set_alarma =document.getElementById("btn_set_alarma");
 btn_set_alarma.onclick=function(){
-    let hora = document.getElementById(inputHora).value;
-    let minut = document.getElementById(inputMinut).value;
-    let segon = document.getElementById(inputSegon).value;
-    ref_window.document.getElementById("div_hora_alarma").innerHTML=hora+":"+minut
+    let hora = document.getElementById("inputHora").value;
+    let minut = document.getElementById("inputMinut").value;
+    let segon = document.getElementById("inputSegon").value;
+    ref_window.document.getElementById("div_hora_alarma").innerHTML=hora+":"+minut+":"+segon;
 
+}
+
+let colorFilla = 'orange;';
+function getColorFilla(){
+    return colorFilla;
+}
+
+function mostrarHoraFilla(){
+    window.setInterval(function(){
+        let hora = new Date();
+        ref_window.document.getElementById("div_hora_actual").innerHTML= hora.getHours()+
+        ":"+hora.getMinutes()+":"+hora.getSeconds();
+    }, 1000)
 }
