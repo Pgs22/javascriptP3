0e89976d317b930618d6e3d9bc0739f2e21707a4
patricia <patricia.gsbcn@gmail.com>
2025-11-20 17:03:52 +0100

    reproductor y dos canciones

--- Exercici02.js
+++ Exercici02.js
@@ -1,19 +1,29 @@
 //creaun array ex2
 
-//guardo la primera cancion
-song1= new Array();
-song1=["titul",".mp3","nom"];
+//nueva cancion
+song = new Array();
+let nom = "nom"
+let tipus = ".mp3"
+let titul = "titul"
+song = [nom, tipus ,nom];
 
-//guardo las canciones añadidas
+//Creo array del reproductor para guardar las canciones
 reproductor= new Array();
-reproductor[0]=song1;
-console.log(song1)
+//guardo cancion song
+reproductor[0]=song;
+//guardo otra cancion
+reproductor[1]=["titulo", "tipo", "nombre"];
+//para ver en la consola
+console.log(song)
 console.log(reproductor)
 
+//Recorrer el array de listado de canciones
+for(let k=0;k<reproductor.length;k++ ){
+    console.log(k+": "+reproductor[k]);
+}
 
-
-/*
-for(let k=0;k<llista_mumeros.length;k++ ){
-    llista_numeros[k]=k;
-
-}*/
+/* Recorrer el array de listado de canciones y detalle de cada cancion */
+for(let k=0;k<reproductor.length;k++ ){
+    for(let m=0;m<reproductor[k].length;m++ )
+        console.log(m+": "+reproductor[k][m]);
+}
